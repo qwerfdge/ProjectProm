1e9b5a21600f876127c011b4b5ad7cd07b4956dd
Anastasia <brehernasti@gmail.com>
2024-03-14 09:36:49 +0200

    commit 29.02.2024

---
+++ src/main/java/org/example/pages/SearchPage.java
@@ -0,0 +1,24 @@
+package org.example.pages;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SearchPage {
+
+    private WebDriver driver;
+    private By searchBox = By.id("Ноутбук");
+
+    public SearchPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void searchFor(String query) {
+        WebElement searchInput = driver.findElement(searchBox);
+        searchInput.sendKeys(query);
+        searchInput.sendKeys(Keys.RETURN);
+    }
+}
---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,37 @@
+
+
+// Язык программирования: Java
+
+import org.example.pages.SearchPage;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SearchTest {
+
+    private WebDriver driver;
+    private SearchPage searchPage;
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://prom.ua/p1272542085-loson-sprej-dlya.html");
+        searchPage = new SearchPage(driver);
+    }
+
+    @Test
+    public void testSiteSearch() {
+        searchPage.searchFor("Ноутбук");
+
+        // Добавьте проверку наличия результатов поиска
+        Assert.assertTrue("Результаты поиска не отображены", searchPage.areSearchResultsDisplayed());
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
